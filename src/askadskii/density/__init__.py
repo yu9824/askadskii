bbd1c0fd6ff814476dca737af4af9b3201e8a94b
yu9824 <58211916+yu9824@users.noreply.github.com>
2024-09-12 12:05:51 +0900

    Move _decrypt .density to .

--- src/askadskii/density/__init__.py
+++ src/askadskii/density/__init__.py
@@ -25,5 +25,3 @@ References
 from ._core import estimate_density, estimate_vdw_volume
 
 __all__ = ("estimate_density", "estimate_vdw_volume")
-
-import askadskii.utils.dataset._decrypt  # noqa: F401
